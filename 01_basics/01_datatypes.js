279caa3d1685be451cccc4bf1a1d910c750cc71d
SHANKET KUMAR <132742876+editorbymood@users.noreply.github.com>
2024-08-25 18:34:23 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";  // treat all JS code as newer version
+
+// alert( 3+3) // we are using node.js,  not browser
+
+console.log(3+3) 
+
+// code readablitiy should be high 
+
+console.log("Shanket")
+
+
+
+let name = "Shanket"
+let age = 21
+let isloggedIn = false
+
+
+
+// primitive data types
+// number => 2 to the power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
